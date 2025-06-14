900939fdcd960e3ceebc10a0e939c8a85d99ebc0
Alice V <ooisee@gmail.com>
2025-06-14 18:17:41 +0100

    fix: update input path in run-wisdom.sh script

--- run-wisdom.sh
+++ run-wisdom.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./chat-transformer -i "/mnt/c/Users/avinogradova/OneDrive/@wisdom/raw/" -o "/mnt/c/Users/avinogradova/OneDrive/@wisdom/expanded/"
+./chat-transformer -i "/mnt/c/Users/avinogradova/@wisdom/raw/" -o "/mnt/c/Users/avinogradova/@wisdom/expanded/"
